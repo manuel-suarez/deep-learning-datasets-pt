7cd29f0c2994c3242ebaa087dad1f9e419ef0b5a
Arturo Suárez <manuel.suarez@cimat.mx>
2024-08-12 20:02:14 -0600

    Added main on each dataset file to test

--- cimat.py
+++ cimat.py
@@ -72,3 +72,42 @@ class CimatDataset(Dataset):
         # x = torch.tensor(x).permute(2, 0, 1)
         # y = torch.tensor(y).permute(2, 0, 1)
         return x, y
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import torchvision.transforms.functional as F
+
+    home_dir = os.path.expanduser("~")
+    feat_channels = ["ORIGIN", "ORIGIN", "VAR"]
+
+    train_dataset = CimatDataset(
+        base_dir=home_dir,
+        dataset="17",
+        trainset="01",
+        features_channels=feat_channels,
+        features_extension=".tiff",
+        labels_extension=".pgm",
+    )
+    image, label = train_dataset[0]
+    print(f"Tensor image shape: {image.shape}")
+    print(f"Tensor label shape: {label.shape}")
+    np_image = image.numpy()
+    np_label = label.numpy()
+    print(f"Numpy image shape: {np_image.shape}")
+    print(f"Numpy label shape: {np_label.shape}")
+
+    print(
+        f"Image: max: {np.max(np_image)}, min: {np.min(np_image)}, values: {np.unique(np_image)}"
+    )
+    print(
+        f"Label: max: {np.max(np_label)}, min: {np.min(np_label)}, values: {np.unique(np_label)}"
+    )
+
+    fig, axs = plt.subplots(1, 2)
+    pil_image = np.array(F.to_pil_image(image))
+    pil_label = np.array(F.to_pil_image(label))
+    axs[0].imshow(pil_image)
+    axs[1].imshow(pil_label)
+    plt.show()
--- krestenitis.py
+++ krestenitis.py
@@ -28,3 +28,36 @@ class KrestenitisDataset(Dataset):
         label = read_image(self.labels_fps[idx])
 
         return image, label
+
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import torchvision.transforms.functional as F
+
+    home_dir = os.path.expanduser("~")
+    data_dir = os.path.join(home_dir, "data", "oil-spill-dataset")
+    train_dir = os.path.join(data_dir, "train")
+
+    train_dataset = KrestenitisDataset(train_dir)
+    image, label = train_dataset[0]
+    print(f"Tensor image shape: {image.shape}")
+    print(f"Tensor label shape: {label.shape}")
+    np_image = image.numpy()
+    np_label = label.numpy()
+    print(f"Numpy image shape: {np_image.shape}")
+    print(f"Numpy label shape: {np_label.shape}")
+
+    print(
+        f"Image: max: {np.max(np_image)}, min: {np.min(np_image)}, values: {np.unique(np_image)}"
+    )
+    print(
+        f"Label: max: {np.max(np_label)}, min: {np.min(np_label)}, values: {np.unique(np_label)}"
+    )
+
+    fig, axs = plt.subplots(1, 2)
+    pil_image = np.array(F.to_pil_image(image))
+    pil_label = np.array(F.to_pil_image(label))
+    axs[0].imshow(pil_image)
+    axs[1].imshow(pil_label)
+    plt.show()
