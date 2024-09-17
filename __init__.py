859a4434b0c264e970c5913e346765796941ef34
Arturo Suárez <manuel.suarez@cimat.mx>
2024-09-17 14:52:51 -0600

    Custom implementation for saving of figures and predictions

--- __init__.py
+++ __init__.py
@@ -1,7 +1,23 @@
-from datasets.cimat import prepare_dataloaders as prepare_cimat_dataloaders
-from datasets.krestenitis import prepare_dataloaders as prepare_krestenitis_dataloaders
-from datasets.sos import prepare_dataloaders as prepare_sos_dataloaders
-from datasets.chn6_cug import prepare_dataloaders as prepare_chn6_dataloaders
+from datasets.cimat import (
+    prepare_dataloaders as prepare_cimat_dataloaders,
+    save_predictions as save_cimat_predictions,
+    save_figures as save_cimat_figures,
+)
+from datasets.krestenitis import (
+    prepare_dataloaders as prepare_krestenitis_dataloaders,
+    save_predictions as save_krestenitis_predictions,
+    save_figures as save_krestenitis_figures,
+)
+from datasets.sos import (
+    prepare_dataloaders as prepare_sos_dataloaders,
+    save_predictions as save_sos_predictions,
+    save_figures as save_sos_figures,
+)
+from datasets.chn6_cug import (
+    prepare_dataloaders as prepare_chn6_dataloaders,
+    save_predictions as save_chn6_predictions,
+    save_figures as save_chn6_figures,
+)
 
 
 def get_dataloaders(base_dir, name, args=None):
@@ -40,3 +56,17 @@ def get_dataloaders(base_dir, name, args=None):
         return prepare_chn6_dataloaders(base_dir)
     # To be implemented
     raise Exception(f"Dataset {name} it's currently not implemented!")
+
+
+def get_savers(name):
+    if name == "cimat":
+        return save_cimat_predictions, save_cimat_figures
+    if name == "krestenitis":
+        return save_krestenitis_predictions, save_krestenitis_figures
+    if name == "sos":
+        return save_sos_predictions, save_sos_figures
+    if name == "chn6-cug":
+        return save_chn6_predictions, save_chn6_figures
+    raise Exception(
+        f"Save predictions and figures functions of dataset {name} it's currently not implemented!"
+    )
--- sos.py
+++ sos.py
@@ -1,6 +1,8 @@
 import os
 import torch
 import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
 from skimage.io import imread
 from torch.utils.data import Dataset, DataLoader
 
@@ -65,6 +67,52 @@ def prepare_dataloaders(base_dir):
         test_dataset, batch_size=4, pin_memory=True, shuffle=False, num_workers=4
     )
     return train_dataloader, valid_dataloader, test_dataloader
+
+
+def save_predictions(batch_idx, images, labels, predictions, directory):
+    for idx_image, image in enumerate(images):
+        # Image in SOS is gray image (one channel)
+        image = np.squeeze(image)
+        # print(f"\t{idx_image} image shape: ", image.shape)
+        # image = np.transpose(image, (1, 2, 0))
+        image = image * 255
+        image = image.astype(np.uint8)
+        image_p = Image.fromarray(image)
+        image_p = image_p.convert("L")
+        image_p.save(os.path.join(directory, f"batch{idx_batch}_image{idx_image}.png"))
+    for idx_label, label in enumerate(labels):
+        # label = np.transpose(label, (1, 2, 0))
+        label = np.squeeze(label)
+        # print(f"\t{idx_label} label shape: ", label.shape)
+        label = label * 255
+        label = label.astype(np.uint8)
+        label_p = Image.fromarray(label)
+        label_p = label_p.convert("L")
+        label_p.save(os.path.join(directory, f"batch{idx_batch}_label{idx_label}.png"))
+    for idx_prediction, prediction in enumerate(predictions):
+        # prediction = np.transpose(prediction, (1, 2, 0))
+        prediction = np.squeeze(prediction)
+        # print(f"\t{idx_prediction} prediction shape: ", prediction.shape)
+        prediction_p = Image.fromarray((prediction * 255).astype(np.uint8))
+        prediction_p = prediction_p.convert("L")
+        prediction_p.save(
+            os.path.join(
+                directory,
+                f"batch{batch_idx}_prediction{idx_prediction}.png",
+            )
+        )
+
+
+def save_figures(batch_idx, images, labels, predictions, directory):
+    fig, axs = plt.subplots(1, 3, figsize=(12, 8))
+    axs[0].imshow(images[0, 0, :, :])
+    axs[0].set_title("Imagen")
+    axs[1].imshow(predictions[0, 0, :, :])
+    axs[1].set_title("Prediction")
+    axs[2].imshow(labels[0, 0, :, :])
+    axs[2].set_title("Label")
+    plt.savefig(os.path.join(directory, f"result_batch{batch_idx}.png"))
+    plt.close()
 
 
 if __name__ == "__main__":
